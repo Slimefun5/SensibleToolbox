639cc5104f119e1c5d236ade344416b2501e3cb3
Intisy <finn@birich.de>
2026-04-27 00:32:43 +0200

    Add JUnit 5 and MockBukkit test scaffolding

---
+++ src/test/java/io/github/thebusybiscuit/sensibletoolbox/TestSensibleToolboxPlugin.java
@@ -0,0 +1,29 @@
+package io.github.thebusybiscuit.sensibletoolbox;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockbukkit.mockbukkit.MockBukkit;
+import org.mockbukkit.mockbukkit.ServerMock;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class TestSensibleToolboxPlugin {
+
+    private ServerMock server;
+
+    @BeforeEach
+    void setUp() {
+        server = MockBukkit.mock();
+    }
+
+    @AfterEach
+    void tearDown() {
+        MockBukkit.unmock();
+    }
+
+    @Test
+    void testServerLoads() {
+        assertNotNull(server);
+    }
+}
